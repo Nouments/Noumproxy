c09891910504fe0c299a2e345dc3c96a5e0a628d
noums <noums@hacker.noums>
2025-07-25 17:04:04 +0300

    adding react with typescript for web-UI

---
+++ internal/web/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-center px-4">
+      <div>
+        <h1 className="text-5xl font-bold mb-4">🚀 NoumProxy</h1>
+        <p className="text-2xl text-gray-300">Coming Soon...</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App

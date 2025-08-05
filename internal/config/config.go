5fcbce860eb47d1f7fe081d99c15d82737e75fa9
noums <noums@hacker.noums>
2025-08-05 23:24:04 +0300

    ajout de parser et de la strucure de  yaml

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Host struct {
+	Address string `yaml:"address"`
+	Port    int    `yaml:"port"`
+	Weight  int    `yaml:"weight"`
+}
+
+type Server struct {
+	Domain  string  `yaml:"domain"`
+	Host    []Host  `yaml:"host"`
+	CertKey string  `yaml:"cert-key"`
+}
